338ccff2ca2a9d1bf44ef074670cd82cec76529c
sevenfate <7fate@web.de>
2016-09-08 11:08:34 +0200

    Spring Dependencies with config xml

--- src/main/java/de/seven/fate/service/PublishEventService.java
+++ src/main/java/de/seven/fate/service/PublishEventService.java
@@ -2,16 +2,26 @@ package de.seven.fate.service;
 
 import de.seven.fate.entity.Order;
 import de.seven.fate.event.CreationEvent;
+import de.seven.fate.rule.OrderRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+import javax.annotation.PostConstruct;
+
+@Component
 public class PublishEventService {
 
+    //Constructor Dependency
     private final ApplicationEventPublisher publisher;
 
+    //Field Dependency
     @Autowired
+    private OrderRule simpleOrderRule;
+
+    //Setter Dependency
+    private OrderValidationServiceImpl complexOrderValidationService;
+
     public PublishEventService(ApplicationEventPublisher publisher) {
         this.publisher = publisher;
     }
@@ -20,4 +30,13 @@ public class PublishEventService {
         publisher.publishEvent(new CreationEvent(order));
     }
 
+    @PostConstruct
+    private void init() {
+        System.out.println(this);
+    }
+
+
+    public void setComplexOrderValidationService(OrderValidationServiceImpl complexOrderValidationService) {
+        this.complexOrderValidationService = complexOrderValidationService;
+    }
 }
